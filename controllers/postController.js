1d594ed898c6f62f04fbe06a2aed447815a5df29
adi161001 <aditya220972@gmail.com>
2023-03-05 17:36:16 +0530

    +/- in votecount

--- controllers/postController.js
+++ controllers/postController.js
@@ -23,3 +23,17 @@ export const getPost = asyncHandler(async (req, res) => {
  
     
 });
+export const incease_votes = asyncHandler(async (req, res) => {
+    
+    let a =await Post.findOne({_id})  //SELECT AND WHERE 
+    a.votes_count=a.votes_count+1      //UPDATE 
+    await a.save();    
+       res.json({success:true})         //https://stackoverflow.com/questions/67043015/mongoose-access-current-value-before-updating-it-inside-findoneandupdate
+   });
+   export const decrease_votes = asyncHandler(async (req, res) => {
+    
+    let a =await Post.findOne({_id})  //SELECT AND WHERE 
+    a.votes_count=a.votes_count-1      //UPDATE 
+    await a.save();    
+       res.json({success:true})         //https://stackoverflow.com/questions/67043015/mongoose-access-current-value-before-updating-it-inside-findoneandupdate
+   });
